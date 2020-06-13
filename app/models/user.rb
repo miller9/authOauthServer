ae00edc04d30006d7e1ee56d53fdf55d3c4a1c35
miller <millerpoli9@gmail.com>
2020-06-13 12:36:39 -0500

    User model. Agrego gema bcrypt, creo funcion para encriptar password. Agrego restricciones para el atributo username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+	require "bcrypt"
+	before_create :set_encrypt_password
+
+	validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+
+
+	def set_encrypt_password
+		self.password = BCrypt::Password.create self.password
+	end
+
 end
